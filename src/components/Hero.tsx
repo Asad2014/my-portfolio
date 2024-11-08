d0cd61a3a4640ef2e7927f0bcca04f6eb31703ae
asad2014 <asadkk2014@gmail.com>
2024-11-09 01:26:22 +0500

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,78 +1,23 @@
 
-//  "use client";
-// import React from 'react'
-// import Image from 'next/image'
-// import Typewriter from 'typewriter-effect';
-
-
-
-
-// const Hero = () => {
-  
-// const someModule = require('some-module');
-
-//   return (
-//     <div>
-//       <section className="text-gray-600 body-font">
-//   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-//     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-//       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-//         Hello, Iam
-//         <br className="hidden lg:inline-block" />
-        
-//         <Typewriter
-//          options={{
-//          strings: ['Asad Iqbal', 'Front-End Developer'],
-//          autoStart: true,
-//          loop: true,
-//        }}
-//      />
-       
-//       </h1>
-//       <p className="mb-8 leading-relaxed text-white">
-//       Passionate and driven aspiring AI enthusiast with a keen interest in the intersection of technology and artificial intelligence. Currently honing my skills in Nextjs to develop expertise in building intelligent systems and applications. Excited to explore the endless possibilities of AI and contribute to groundbreaking innovations that positively impact society.
-//       </p>
-      
-//     </div>
-//        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-//   <Image
-//     className="object-cover object-center rounded mx-auto"
-//     alt="profile-picture"
-//     src={require('../../public/pic.jpeg')}
-//     width={320} 
-//     height={267} 
-//     layout="intrinisc" 
-//   />
-// </div>
-
-//   </div>
-// </section>
-
-//     </div>
-//   )
-// }
-
-// export default Hero
 
 import React from 'react'
 
-
 const Hero = () => {
   return (
-    <div id="hero" className='min-h-screen bg-no-repeat bg-[url(/pic.jpeg)]  bg-cover'
-    style={{backgroundSize: "25%" , backgroundPosition: "left 150px top 150px" }}
-    >    
-    
-      <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
-      <div className='hidden lg:block'></div>
-      <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
-      <div>
-        <p>I'm</p>
-        <p>Asad</p>
-        <p>Iqbal</p>
-        
-      </div>
-      </div>
+    <div
+      id="hero"
+      className="min-h-screen bg-no-repeat bg-[url(/pic.jpeg)] bg-cover"
+      style={{ backgroundSize: "25%", backgroundPosition: "left 150px top 150px" }}
+    >
+      <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
+        <div className="hidden lg:block"></div>
+        <div className="text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center">
+          <div>
+            <p>Iam</p> 
+            <p>Asad</p>
+            <p>Iqbal</p>
+          </div>
+        </div>
       </div>
     </div>
   )
